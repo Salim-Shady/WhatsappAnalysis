0faee4b4b3edb73a466f51fcbcd3e59c03a54e84
BSalim <burhan.salman@gmail.com>
2018-07-17 20:30:05 +0400

    add js to put chat into array

--- analyse.php
+++ analyse.php
@@ -8,11 +8,31 @@
         echo "<meta http-equiv=\"refresh\" content=\"0; url=./index.html\" />";
         exit();
       }
-      $file = $_FILES['file'];
-      move_uploaded_file($file['tmp_name'], "uploads/".$file['name']);
+      $fileArr = $_FILES['file'];
+      $fileLoc = "uploads/".$fileArr['name'];
+      move_uploaded_file($fileArr['tmp_name'], $fileLoc);
+
+      $file = fopen($fileLoc, 'r');
+      $fileData = fread($file, filesize($fileLoc));
+      $fileData = htmlspecialchars($fileData);
+      $fileData = nl2br($fileData);
     ?>
   </head>
   <body>
+    <div id="chatText">
+      <?php echo $fileData; ?>
+    </div>
+    <script>
+      let chatText = document.getElementById('chatText').innerHTML;
+      chatText = chatText.replace(/<br>/g, '');
 
+      //regex to pick out only chats
+      let exp = /\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2} - .*:.*/g;
+      let arr, messages = [];
+      //while there are more chats add them to messages
+      while ((arr = exp.exec(chatText)) != null) {
+        messages.push(arr[0]);
+      }
+    </script>
   </body>
 </html>
