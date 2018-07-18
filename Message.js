4b41fe34211275ca08383c62b0c83a2a9207b4f7
BSalim <burhan.salman@gmail.com>
2018-07-18 20:22:46 +0400

    add compareTo

--- Message.js
+++ Message.js
@@ -1,5 +1,5 @@
 class Message {
-  constructor(dateString, time, sender, text) {
+  constructor(dateString, sender, text) {
 
     let dateRegex = /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2})/g
     let dateArr = dateRegex.exec(dateString);
@@ -14,4 +14,16 @@ class Message {
     this.sender = sender;
     this.text = text;
   }
+
+  compareTo(otherMessage) {
+    if (!(otherMessage instanceof Message)) return;
+
+    if (this.date > otherMessage.date) return 1;
+    if (this.date < otherMessage.date) return -1;
+    if (this.date.getTime() == otherMessage.date.getTime()) return 0;
+
+    return null;
+  }
+
+
 }
