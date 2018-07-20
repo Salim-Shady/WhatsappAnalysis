d5193a08583f097e2c43a9a3761aed5fd3cf4efb
BSalim <burhan.salman@gmail.com>
2018-07-20 23:41:58 +0400

    add stats obj

--- getStats.js
+++ getStats.js
@@ -1,7 +1,55 @@
+/*
+  messagesObj contains all the messages as Message objects from the chat
+*/
+//stats object will hold all the stats of the chat
+let stats = {
+  totalMsgs: 0,
+  msgPercentage: {},
+  maxMsg: {
+    senders: {},
+    overall: null
+  },
+  minMsg: {
+    senders: {},
+    overall: null
+  },
+  firstMsg: {
+    sender: null,
+    msg: null
+  },
+  longestReign: {
+    sender: null,
+    messages: []
+  },
+  longestIdle: {
+    sender: null,
+    idleLength: 0
+  },
+  mostChars: {
+    sender: null
+  },
+  leastChars:
+};
+
 let totalMsg = messageObj.length;
+stats.totalMessages = totalMsg;
 let senders = new Set();
 
 //add all senders to set
 for (let i = 0; i < totalMsg; i++) {
   senders.add(messageObj[i].sender);
 }
+
+//make objects for all senders
+let sendersArr = [...senders];
+let sendersObj = [];
+for (sender in sendersArr) {
+  sendersObj.push(new Sender(sender));
+}
+
+//add the messages to respective senders
+for (messages in messagesObj) {
+
+}
+
+//calcu
