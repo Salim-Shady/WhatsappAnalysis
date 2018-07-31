71849590ceef6a1c8cff65e4942614d1d67fec66
BSalim <burhan.salman@gmail.com>
2018-07-31 22:11:38 +0400

    update the date storing to fit sql

--- js/Message.js
+++ js/Message.js
@@ -4,27 +4,28 @@ class Message {
     let dateRegex = /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2})/g
     let dateArr = dateRegex.exec(dateString);
 
-    let day = parseInt(dateArr[1]);
-    let month = parseInt(dateArr[2]) - 1;
-    let year = parseInt(dateArr[3]);
-    let hour = parseInt(dateArr[4]);
-    let mins = parseInt(dateArr[5]);
+    // let day = parseInt(dateArr[1]);
+    // let month = parseInt(dateArr[2]) - 1;
+    // let year = parseInt(dateArr[3]);
+    // let hour = parseInt(dateArr[4]);
+    // let mins = parseInt(dateArr[5]);
 
     this.dateString = dateString;
-    this.date = new Date(year,month,day,hour,mins);
+    this.sqlDate = dateArr[3]+'-'+dateArr[2]+'-'+dateArr[1]+' '+dateArr[4]+':'+dateArr[5]+':00';
+    // this.date = new Date(year,month,day,hour,mins);
     this.sender = sender;
     this.text = text;
   }
 
-  compareTo(otherMessage) {
-    if (!(otherMessage instanceof Message)) return;
+  // compareTo(otherMessage) {
+  //   if (!(otherMessage instanceof Message)) return;
 
-    if (this.date > otherMessage.date) return 1;
-    if (this.date < otherMessage.date) return -1;
-    if (this.date.getTime() == otherMessage.date.getTime()) return 0;
+  //   if (this.date > otherMessage.date) return 1;
+  //   if (this.date < otherMessage.date) return -1;
+  //   if (this.date.getTime() == otherMessage.date.getTime()) return 0;
 
-    return null;
-  }
+  //   return null;
+  // }
 
   toString() {
     return this.dateString + " - " + this.sender + ": " + this.text;
