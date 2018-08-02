f4cbb24db069721ed1dc7beba610a6e99da0ed52
BSalim <burhan.salman@gmail.com>
2018-08-02 15:36:25 +0400

    add connecting to database

--- php/databaseDetails.php
+++ php/databaseDetails.php
@@ -2,3 +2,7 @@
   $dbHost = "localhost";
   $dbUser = "root";
   $dbPass = "";
+  $dbName = "whatsappanalysis";
+  //create mysqli connection
+  $conn = new mysqli($dbHost,$dbUser,$dbPass);
+  !$conn->connect_error OR die();
