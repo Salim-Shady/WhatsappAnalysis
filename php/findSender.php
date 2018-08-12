dd6709b50ceb64e7489aad89eaf632da4fab8773
BSalim <burhan.salman@gmail.com>
2018-08-12 20:53:26 +0400

    add query to retrive sender stats

--- php/findSender.php
+++ php/findSender.php
@@ -1,4 +1,13 @@
 <?php
   require_once('databaseDetails.php');
 
-  $queryFindSenders = "SELECT DISTINCT "
+  $queryFindSenders = "SELECT sender, COUNT(message) AS count FROM $dbName.Messages GROUP BY sender";
+  $res = $conn->query($queryFindSenders);
+
+  $rows = array();
+  while ($row = $res->fetch_assoc()) {
+    $rows[] = $row;
+  }
+
+  print json_encode($rows, JSON_UNESCAPED_UNICODE);
+  $conn->close();
