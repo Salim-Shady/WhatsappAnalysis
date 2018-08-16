6d479c36453c3a7c0db1643dcf3071d22ad20067
BSalim <burhan.salman@gmail.com>
2018-08-16 04:24:40 +0400

    add queries for date monthh and year

--- php/findFreq.php
+++ php/findFreq.php
@@ -1,13 +1,29 @@
 <?php
   require_once('databaseDetails.php');
   
-  //query retrieves the frequency of messages per day
+  //query frequency of messages by day
   $queryFreqDays = "SELECT DAYNAME(date) AS day, COUNT(DAYNAME(date)) AS count FROM $dbName.messages GROUP BY day";
   $freqDays = $conn->query($queryFreqDays);
 
-  //query retrives frequency of messages in every hour
+  //query frequency of messages by hour
   $queryFreqHours = "SELECT HOUR(time) AS hour, COUNT(HOUR(time)) AS count FROM $dbName.messages GROUP BY hour";
   $freqHours = $conn->query($queryFreqHours);
+
+  //query frequency of messages by date
+  $queryFreqDate = "SELECT date AS date, COUNT(date) AS count FROM $dbName.messages GROUP BY date";
+  $freqDate = $conn->query($queryFreqDate);
+
+  //query frequency of messages by year
+  $queryFreqYear = "SELECT YEAR(date) AS year, SUM(count) AS count 
+                    FROM ($queryFreqDate) AS queryFreqDate 
+                    GROUP BY year";
+  $freqYear = $conn->query($queryFreqYear);
+
+  //query frequency of messages by month
+  $queryFreqMonth = "SELECT MONTH(date) AS month, SUM(count) AS count 
+                     FROM ($queryFreqDate) AS queryFreqDate
+                     GROUP BY month";
+  $freqMonth = $conn->query($queryFreqMonth);
 
   //put all results into arrays
   $arrfreqDays = array();
@@ -20,13 +36,47 @@
     $arrfreqHours[] = $row;
   }
 
+  $resFreqDates = array();
+  while($row = $freqDate->fetch_assoc()) {
+    $resFreqDates[] = $row;
+  }
+
+  $resFreqYears = array();
+  while($row = $freqYear->fetch_assoc()) {
+    $resFreqYears[] = $row;
+  }
+
+  $arrFreqMonths = array();
+  while($row = $freqMonth->fetch_assoc()) {
+    $arrFreqMonths[] = $row;
+  }
+
   //fill the arrays so all days and hours have values
-  //all the values to be filled
   $days = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
   $hours = range(0,23);
+  $months = range(1,12);
 
   $resFreqDays = fillValues($arrfreqDays,$days,'day');
   $resFreqHours = fillValues($arrfreqHours,$hours,'hour');
+  $resFreqMonths = fillValues($arrFreqMonths,$months,'month');
+
+  //print to output
+  $result = array(
+    'day'=>$resFreqDays,
+    'hour'=>$resFreqHours,
+    'date'=>$resFreqDates,
+    'year'=>$resFreqYears,
+    'month'=>$resFreqMonths
+  );
+
+  print json_encode($result, JSON_UNESCAPED_UNICODE);
+  $conn->close();
+
+
+
+
+
+
 
 
   //returns an array that is filled with all the data from the filler
@@ -51,7 +101,4 @@
     return $array;
   }
 
-  $result = array($resFreqDays,$resFreqHours);
 
-  print json_encode($result, JSON_UNESCAPED_UNICODE);
-  $conn->close();
