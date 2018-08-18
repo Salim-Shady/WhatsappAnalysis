95da578c4d0fbcb21295b829d2ac0c073546971d
BSalim <burhan.salman@gmail.com>
2018-08-18 18:44:58 +0400

    change async to promises

--- js/database.js
+++ js/database.js
@@ -11,55 +11,79 @@ $.ajax({
   async:false
 });
 
-//post to php to retrieve sender stats
-let total;
-$.ajax({
-  type:'POST',
-  url: 'php/findSender.php',
-  success: function(res) {
-    total = JSON.parse(res);    
-  },
-  async: true
-});
 
-//post to php to retrive longest and shortest message
-let minMaxMessage;
-$.ajax({
-  type:'POST',
-  url: 'php/findMaxMin.php',
-  success: function(res) {
-    // alert(res);
-    minMaxMessage = JSON.parse(res);    
-  },
-  async: true
-});
+let findTotal = () => {
+  return new Promise((resolve,reject) => {
+    let ans;
+    $.ajax({
+      type:'POST',
+      url: 'php/findSender.php',
+      success: function(res) {
+        ans = JSON.parse(res);    
+      },
+      async: false
+    });
 
-let frequency;
-$.ajax({
-  type:'POST',
-  url: 'php/findFreq.php',
-  success: function(res) {
-    // alert(res);
-    frequency = JSON.parse(res);    
-  },
-  async: false
-});
+    resolve(ans);
+  });
+};
 
-//sort frequency by day
-frequency.day.sort(function(a,b) {
-  let order = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  let indexA = order.indexOf(a.day);
-  let indexB = order.indexOf(b.day);
+let findMinMax = () => {
+  return new Promise((resolve,reject) => {
+    let ans;
+    $.ajax({
+      type:'POST',
+      url: 'php/findMaxMin.php',
+      success: function(res) {
+        console.log(res);
+        ans = JSON.parse(res);    
+      },
+      async: false
+    });
 
-  return indexA - indexB;
-});
+    resolve(ans);
+  });
+};
 
-//sort frequency by hour
-frequency.hour.sort(function(a,b) {
-  return a.hour - b.hour;
-});
+let findFreq = () => {
+  return new Promise((resolve,reject) => {
+    let ans;
+    $.ajax({
+      type:'POST',
+      url: 'php/findFreq.php',
+      success: function(res) {
+        ans = JSON.parse(res);    
+      },
+      async: false
+    });
 
-//sort frequency by month
-frequency.month.sort(function(a,b) {
-  return a.month - b.month;
-});
+    //sort ans by day
+    ans.day.sort(function(a,b) {
+      let order = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      let indexA = order.indexOf(a.day);
+      let indexB = order.indexOf(b.day);
+
+      return indexA - indexB;
+    });
+
+    //sort ans by hour
+    ans.hour.sort(function(a,b) {
+      return a.hour - b.hour;
+    });
+
+    //sort ans by month
+    ans.month.sort(function(a,b) {
+      return a.month - b.month;
+    });
+
+    resolve(ans);
+  });
+};
+
+
+let total,minMaxMessage,frequency;
+Promise.all([
+  findTotal().then(success => total = success),
+  findMinMax().then(success => minMaxMessage = success),
+  findFreq().then(success => frequency = success)
+]).then(success => console.log("All queries executed"));
